3faa1f1f0661ecbd93744289513a583f75bac762
Joshua <jpe7619@gmail.com>
2023-09-12 15:14:08 -0700

    complete and test endpoints for PATCH and DELETE

--- GPBackEnd/routes/golfclubs.js
+++ GPBackEnd/routes/golfclubs.js
@@ -11,8 +11,8 @@ router.get('/', async (req, res) => {
   }
 })
 //get one
-router.get('/:id', (req, res) => {
-  res.send(req.params.id)
+router.get('/:id', getGolfclub, (req, res) => {
+  res.send(res.golfclub)
 })
 //create one
 router.post('/', async (req, res) => {
@@ -29,12 +29,46 @@ router.post('/', async (req, res) => {
   }
 })
 //update one
-router.patch('/:id', (req, res) => {
+router.patch('/:id', getGolfclub, async (req, res) => {
+  if (req.body.name != null) {
+    res.golfclub.name = req.body.name;
+  }
+  if (req.body.distance != null) {
+    res.golfclub.distance = req.body.distance;
+  }
+  if (req.body.placement != null) {
+    res.golfclub.placement = req.body.placement;
+  }
 
-})
+  try {
+    const updatedGolfclub = await res.golfclub.save();
+    res.json(updatedGolfclub);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 //delete one
-router.delete('/:id', (req, res) => {
+router.delete('/:id', getGolfclub, async (req, res) => {
+  try {
+    await res.golfclub.deleteOne();
+    res.json({ message: 'Deleted Golfclub' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-})
+//middleware function
+async function getGolfclub(req, res, next) {
+  try {
+    golfclub = await Golfclub.findById(req.params.id)
+    if (golfclub == null) {
+      return res.status(404).json({ message: 'Cannot find golfclub'})
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message})
+  }
+  res.golfclub = golfclub
+  next()
+}
 
 module.exports = router
