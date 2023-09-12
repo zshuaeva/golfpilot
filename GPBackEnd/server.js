099646acf088fe056ef7f43b3b1e0d5486a1bb92
Joshua <jpe7619@gmail.com>
2023-09-12 15:53:41 -0700

    Create Clubguide frontend and handle CORS policy

--- GPBackEnd/server.js
+++ GPBackEnd/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
@@ -10,6 +11,7 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
+app.use(cors())
 
 const golfclubsRouter = require('./routes/golfclubs')
 app.use('/golfclubs', golfclubsRouter)
