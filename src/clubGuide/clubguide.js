be7b32fb69417c9b0f4f6e187bf3af710450de47
Joshua <jpe7619@gmail.com>
2023-09-15 17:23:57 -0700

    update club guide component with ball placement graph

--- src/clubGuide/clubguide.js
+++ src/clubGuide/clubguide.js
@@ -1,37 +1,129 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './clubguide.css'
+import All from './All.png'
+import Driver from './Driver.png'
+import Long from './Long.png'
+import Mid from './Mid.png'
+import Short from './Short.png'
 
 function ClubGuide() {
-  const [clubs, setClubs] = useState([]);
-  const [selectedClub, setSelectedClub] = useState({ distance: '', placement: '' });
 
-  async function fetchGolfClubs() {
-    try {
-      const response = await fetch('http://localhost:3000/golfclubs');
-      const data = await response.json();
-      setClubs(data);
-    } catch (error) {
-      console.error('Error fetching golf clubs:', error);
-    }
-  }
+  const tempClubs = [
+    {
+      name: 'All',
+      distance: '60 yds - 200 yds',
+      position: All
+    },
+    {
+    name: 'Driver',
+    distance: '220 yds',
+    position: Driver
+  },
+  {
+    name: '2 Wood',
+    distance: '215 yds',
+    position: Long
+  },
+  {
+    name: '3 Wood',
+    distance: '210 yds',
+    position: Long
+  },
+  {
+    name: '4 Wood',
+    distance: '205 yds',
+    position: Long
+  },
+  {
+    name: '2 Iron',
+    distance: '180 yds',
+    position: Long
+  },
+  {
+    name: '3 Iron',
+    distance: '170 yds',
+    position: Long
+  },
+  {
+    name: '4 Iron',
+    distance: '160 yds',
+    position: Long
+  },
+  {
+    name: '5 Iron',
+    distance: '155 yds',
+    position: Mid
+  },
+  {
+    name: '6 Iron',
+    distance: '145 yds',
+    position: Mid
+  },
+  {
+    name: '7 Iron',
+    distance: '140 yds',
+    position: Mid
+  },
+  {
+    name: '8 Iron',
+    distance: '130 yds',
+    position: Short
+  },
+  {
+    name: '9 Iron',
+    distance: '115 yds',
+    position: Short
+  },
+  {
+    name: 'Pitching W',
+    distance: '100 yds',
+    position: Short
+  },
+  {
+    name: 'Gap W',
+    distance: '90 yds',
+    position: Short
+  },
+  {
+    name: 'Sand W',
+    distance: '80 yds',
+    position: Short
+  },
+  {
+    name: 'Lob W',
+    distance: '60 yds',
+    position: Short
+  },
 
-  useEffect(() => {
-    fetchGolfClubs();
-  }, []);
+]
+  // async function fetchGolfClubs() {
+  //   try {
+  //     const response = await fetch('http://localhost:3000/golfclubs');
+  //     const data = await response.json();
+  //     setClubs(data);
+  //   } catch (error) {
+  //     console.error('Error fetching golf clubs:', error);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   fetchGolfClubs();
+  // }, []);
+  const [selectedClub, setSelectedClub] = useState({ name: '', distance: '' });
 
   return (
     <div className="clubguide-container">
       <h1>Club Guide</h1>
       <select
         onChange={(e) => {
-          const selectedClubId = e.target.value;
-          const selectedClubDetails = clubs.find((club) => club._id === selectedClubId);
-          setSelectedClub(selectedClubDetails);
+          const selectedClubName = e.target.value;
+          const selectedClubDetails = tempClubs.find((club) => club.name === selectedClubName);
+          setSelectedClub(selectedClubDetails || { name: '', distance: '' });
         }}
       >
         <option value="">Select a club</option>
-        {clubs.map((club) => (
-          <option key={club._id} value={club._id}>
+        {tempClubs.map((club) => (
+          <option key={club.name} value={club.name}>
             {club.name}
           </option>
         ))}
@@ -40,7 +132,8 @@ function ClubGuide() {
         <h2>Selected Club Details</h2>
         <p>Club Name: {selectedClub.name}</p>
         <p>Median Distance: {selectedClub.distance}</p>
-        <p>Ball Placement: {selectedClub.placement}</p>
+        <p>Ball Placement:</p>
+        <img src={selectedClub.position} alt="Foot Position" className="BallPlacementImg" />
       </div>
     </div>
   );
